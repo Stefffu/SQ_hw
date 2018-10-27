8410206c646e7fbaf23614182f1e51132e87ac6a
Steven Leego <steven.leego@gmail.com>
2018-10-27 16:02:05 +0300

    New test case - creating company and adding competence

--- hw/test/hw_test.exs
+++ hw/test/hw_test.exs
@@ -9,4 +9,19 @@ defmodule HwTest do
     end_date = "05.02.2018"
     assert %{Name: name, Desc: description, StDt: start_date, EnDt: end_date} = Hw.createProject(name, description, start_date, end_date) 
   end
+
+  test "Create Company" do
+    name = "TestName"
+    description = "TestDescription"
+    competences = ["Software Development", "Analysis"]
+    assert %{Name: name, Desc: description, Comps: competences} = Hw.createCompany(name, description, competences) 
+  end
+
+  test "Add competence" do
+    name = "TestName"
+    description = "TestDescription"
+    competences = ["Software Development", "Analysis"]
+    competence = "Testing"
+    assert %{Name: name, Desc: description, Comps: ["Software Development", "Analysis", "Testing"]} = Hw.addCompetence(Hw.createCompany(name, description, competences), competence) 
+  end
 end
