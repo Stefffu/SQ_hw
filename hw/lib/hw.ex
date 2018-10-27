af231c35c21882d8c00cbc3ad4fabc763ff3a9a9
Steven Leego <steven.leego@gmail.com>
2018-10-27 16:20:53 +0300

    Code written/refactored. Tests pass. Cycle 2 finished

--- hw/lib/hw.ex
+++ hw/lib/hw.ex
@@ -7,4 +7,12 @@ defmodule Hw do
   def createProject(name, desc, startDate, endDate) do
     %{Name: name, Desc: desc, StDt: startDate, EnDt: endDate}
   end
+
+  def createCompany(name, desc, comps) do
+    %{Name: name, Desc: desc, Comps: comps}
+  end
+
+  def addCompetence(%{Name: name, Desc: desc, Comps: comps}, competence) do
+    %{Name: name, Desc: desc, Comps: comps ++ [competence]}
+  end
 end
