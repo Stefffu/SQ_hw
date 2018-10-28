1e2b54521c625c2cbee867014c1b8fb6a27efa2c
Steven Leego <steven.leego@gmail.com>
2018-10-28 19:20:33 +0200

    Coverage hw tests and code done with coverage assessed

--- coverage_hw/lib/coverage_hw.ex
+++ coverage_hw/lib/coverage_hw.ex
@@ -3,4 +3,60 @@ defmodule CoverageHw do
   def hello do
     :world
   end
+
+  def fun1(value) do
+    (value + value)
+  end
+
+  def loopDeLoop1(msg, _) when msg == "The Game" do
+    IO.puts "You won the game"
+    "won"
+  end
+  
+  def loopDeLoop1(msg, n) when n <= 1 do
+    IO.puts msg
+  end
+
+  def loopDeLoop1(_, n) when n > 10 do
+    IO.puts "Too many loops for this program"
+  end
+
+  def loopDeLoop1(msg, n) when n == 7 do
+      IO.puts "Lucky message: " <> msg
+      loopDeLoop1(msg, n-1)
+  end
+  
+  def loopDeLoop1(msg, n) do
+    IO.puts msg
+    loopDeLoop1(msg, n-1)
+  end
+
+  def loopDeLoop2(msg) when msg == "" do
+    "empty"
+  end
+  
+  def loopDeLoop2(msg) do
+    IO.puts msg
+    loopDeLoop2(String.slice(msg,0,String.length(msg)-1))
+  end
+
+  def loopDeLoop3(number) when number<1 do
+    number
+  end
+  def loopDeLoop4(number) when number<1 do
+    number
+  end
+  def loopDeLoop3(number) do
+    loopDeLoop4(number-2)
+  end
+  def loopDeLoop4(number) do
+    loopDeLoop3(number+1)
+  end
+
+  
+
+
+
+
+
 end
--- coverage_hw/test/coverage_hw_test.exs
+++ coverage_hw/test/coverage_hw_test.exs
@@ -5,4 +5,26 @@ defmodule CoverageHwTest do
   test "greets the world" do
     assert CoverageHw.hello() == :world
   end
+
+  test "LoopDeLoop1" do
+    assert CoverageHw.loopDeLoop1("seven77", 9) == :ok
+    assert CoverageHw.loopDeLoop1("seven77", 11) == :ok
+    assert CoverageHw.loopDeLoop1("The Game", 5) == "won"
+  end
+
+  test "LoopDeLoop2" do
+    assert CoverageHw.loopDeLoop2("test123") == "empty"
+  end
+
+  test "loops 3 and 4" do
+    assert CoverageHw.loopDeLoop3(3) == 0
+    assert CoverageHw.loopDeLoop4(4) == 0
+    assert CoverageHw.loopDeLoop3(-3) == -3
+
+  end
+
+  test "fun1" do
+    assert CoverageHw.fun1(3) == 6
+  end
+
 end
